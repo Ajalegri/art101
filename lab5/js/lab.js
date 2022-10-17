336192e0a1d09e09c7690c1133a44e0cfdbb0895
Ajalegri <78326128+Ajalegri@users.noreply.github.com>
2022-10-17 15:03:11 -0700

    Did Lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ * Author : Antonio Alegria <agalegri@ucsc.edu>
+ * Created: 17 October
+ * License: Public Domain
+ */
+
+// Define Variables
+var make = "Mazda";
+var model = "Miata";
+var color = "Red";
+var year = 1996;
+
+// Calculate
+var age = 2022 - year;
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
