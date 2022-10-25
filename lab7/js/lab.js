398e961a40709743cce73064858e9e0928ef21cb
Ajalegri <78326128+Ajalegri@users.noreply.github.com>
2022-10-24 20:52:55 -0700

    Lab 7 Better

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -5,16 +5,25 @@
  * Using an implementation of the Durstenfeld shuffle from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
  */
 
+// sortUserName: Takes user input and sorts it, capitalizing it properly and removing spaces.
+function sortUserName() {
+
+  // sort
+  var nameArraySorted = nameArray.sort();
+  console.log("nameArraySorted = ", nameArraySorted);
+
+  // convert array back into string, capitalize first letter
+  var nameSorted = nameArraySorted.join('');
+  nameSorted = nameSorted.charAt(0).toUpperCase() + nameSorted.slice(1);
+  console.log("nameSorted = ", nameSorted);
+
+  //output
+  document.writeln(nameSorted, "</br>");
+  return;
+}
+
 // randomizeUserName: Takes user input and randomizes it, capitalizing it properly and removing spaces.
 function randomizeUserName() {
-  // Take user name, convert to lowercase, and remove spaces
-  var userName = window.prompt("Your name sucks. Give it to me so I can fix it.");
-  userName = userName.replace(/\s/g, '').toLowerCase();
-  console.log("userName = ", userName);
-
-  // convert string into array
-  var nameArray = userName.split('');
-  console.log("nameArray = ", nameArray);
 
   // randomize
   var nameArrayRandom = nameArray;
@@ -29,8 +38,16 @@ function randomizeUserName() {
   nameRandom = nameRandom.charAt(0).toUpperCase() + nameRandom.slice(1);
   console.log("nameRandom = ", nameRandom);
 
-  return nameRandom;
+  //output
+  document.writeln(nameRandom, "</br>");
+  return;
 }
 
-// Output
-document.writeln(randomizeUserName(), "</br>");
+// Take user name, convert to lowercase, and remove spaces
+var userName = window.prompt("Your name sucks. Give it to me so I can fix it.");
+userName = userName.replace(/\s/g, '').toLowerCase();
+console.log("userName = ", userName);
+
+// convert string into array
+var nameArray = userName.split('');
+console.log("nameArray = ", nameArray);
